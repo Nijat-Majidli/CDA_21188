ac0121ac044c19158655ff9a53ad44ccffc3116a
nijat <nijatmajidli@gmail.com>
2021-12-07 14:50:13 +0100

    filrouge

--- Fil_Rouge/filRouge.sql
+++ Fil_Rouge/filRouge.sql
@@ -114,17 +114,18 @@ CREATE TABLE commande(
    mode_paiement VARCHAR(50) NOT NULL,
    expedition VARCHAR(50) NOT NULL,
    date_facturation DATE,
+   date_livraison DATE,
    cli_référence INT NOT NULL,
    PRIMARY KEY(com_id),
    FOREIGN KEY(cli_référence) REFERENCES client(cli_référence)
 );
 
-INSERT INTO `commande` (`com_id`, `com_date`, `mode_paiement`, `expedition`, `date_facturation`, `cli_référence`) VALUES 
-(1, '2021-10-21', 'en différé', 'partiellement', '2021-11-05', 1),
-(2, '2021-09-11', 'en différé', 'partiellement', '2021-09-10', 2),
-(3, '2021-08-05', 'en différé', 'partiellement', '2021-09-01', 3),
-(4, '2021-07-18', 'à la commande', 'complet', '2021-08-11', 5),
-(5, '2021-07-23', 'à la commande', 'complet', '2021-08-11', 6);
+INSERT INTO `commande` (`com_id`, `com_date`, `mode_paiement`, `expedition`, `date_facturation`,  `date_livraison`, `cli_référence`) VALUES 
+(1, '2021-10-21', 'en différé', 'partiellement', '2021-10-25', '2021-11-01', 1),
+(2, '2021-09-11', 'en différé', 'partiellement', '2021-09-15', '2021-09-18', 2),
+(3, '2021-08-05', 'en différé', 'partiellement', '2021-08-10', '2021-08-15', 3),
+(4, '2021-07-18', 'à la commande', 'complet', '2021-07-23', '2021-07-25', 5),
+(5, '2021-07-23', 'à la commande', 'complet', '2021-07-26', '2021-07-31', 6);
 
 
 
